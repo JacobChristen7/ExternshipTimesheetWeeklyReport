7e89c4aec95c1ce742522de0cf8702a3fe34aeff
Jacob Christensen <jacobchristensen811@gmail.com>
2025-11-06 14:58:54 -0700

    Initial project setup with Angular, Material, Firebase, and Tailwind

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDPLBp0NKeqfxHLz89HjdrjACaT44b4NEk",
+    authDomain: "externshiptimesheetreport.firebaseapp.com",
+    projectId: "externshiptimesheetreport",
+    storageBucket: "externshiptimesheetreport.firebasestorage.app",
+    messagingSenderId: "460023148589",
+    appId: "1:460023148589:web:6035829a551a0bd0adc8a4",
+    measurementId: "G-XYKM87TTS1"
+  }
+};
