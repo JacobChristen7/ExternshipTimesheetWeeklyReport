bc0131a014007cdd5b4d40876a798e44802f5dd4
Jacob Christensen <jacobchristensen811@gmail.com>
2025-11-19 15:11:00 -0700

    added a dark mode toggle button to the navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  isDarkMode = signal(false);
 
+  toggleDarkMode() {
+    this.isDarkMode.set(!this.isDarkMode());
+    if (this.isDarkMode()) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
