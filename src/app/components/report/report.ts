b28e38c877b8ba92111a239d066a5c591d39c4e8
JacobChristen7 <jacobchristensen811@gmail.com>
2025-12-19 15:19:36 -0700

    added the layout for the report page form along with a simple submit button

--- src/app/components/report/report.ts
+++ src/app/components/report/report.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './report.html',
   styleUrl: './report.css',
 })
 export class Report {
-
+  email = '';
+  week = '';
+  studentName = '';
+  manager = '';
+  company = '';
+  programmingLanguages = '';
+  accomplishments = '';
+  challenges = '';
+  goals = '';
+  hoursThisWeek = '';
+  totalHours = '';
+  questions = '';
+  
+  onSubmit() {
+    console.log('Form submitted:', {
+      email: this.email,
+      week: this.week,
+      studentName: this.studentName,
+      manager: this.manager,
+      company: this.company,
+      programmingLanguages: this.programmingLanguages,
+      accomplishments: this.accomplishments,
+      challenges: this.challenges,
+      goals: this.goals,
+      hoursThisWeek: this.hoursThisWeek,
+      totalHours: this.totalHours,
+      questions: this.questions
+    });
+  }
 }
